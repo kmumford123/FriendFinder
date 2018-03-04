69c3ba979329af718c4f831f92a5f8b1a0844dbc
Khamel <khamel.mumford@gmail.com>
2018-03-04 07:58:40 -0600

    working on routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-[{
+module.exports = [{
         "name": "Will",
         "photo": "https://goo.gl/images/NL1FxB",
         "scores": [
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,5 +1,6 @@
 var path = require("path");
 var friendsData = require("../app/data/friends.js");
+// console.log(friendsData);
 
 
 module.exports = function(app) {
@@ -8,7 +9,7 @@ module.exports = function(app) {
         res.json(friendsData);
     });
     app.post("/api/friends", function(req, res) {
-
+        res.json("friendsData");
         console.log(friendsData);
     });
 };
